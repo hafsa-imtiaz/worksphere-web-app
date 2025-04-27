8a56271735c8368face74cfcf5d340f10f6fa562
mahumhamid <i221009@nu.edu.pk>
2025-04-27 17:19:51 +0500

    Added /project route to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/landing';
 import Login from './pages/login';
 import Signup from './pages/signup';
-import UserSettings from './pages/settings'; 
+import UserSettings from './pages/settings';
+import ProjectManagement from './pages/project';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/settings" element={<UserSettings />} />
+        <Route path="/project" element={<ProjectManagement />} /> 
       </Routes>
     </Router>
   );
