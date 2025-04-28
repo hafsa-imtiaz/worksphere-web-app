1f05e3e333744939bdfac9d92095c6dcbafa956a
hafsa-imtiaz <Hafsa7076@gmail.com>
2025-04-28 20:09:33 +0500

    Frontend Restructuring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,9 @@ import Dashboard from './pages/dashboard';
 import MyCalendar from './pages/MyCalendar.js'
 import { DarkModeProvider } from './contexts/DarkModeContext';
 import Project from './pages/project.js'
-import UserProfile from './pages/settings.js';
+import UserProfile from './pages/UserSettings.js';
+import Inbox from './pages/Inbox.js';
+import MyTasks from './pages/MyTask.js';
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
         <Route path='/calendar' element={<MyCalendar />} />
         <Route path='/project/:id' element={<Project />} />
         <Route path="/profile" element={<UserProfile />} />
+        <Route path="/inbox" element={<Inbox />} />
+        <Route path="/tasks" element={<MyTasks />} />
       </Routes>
     </Router>
   );
